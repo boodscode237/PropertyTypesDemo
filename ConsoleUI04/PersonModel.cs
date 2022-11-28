fec0c3ba4215e11ed2f162e416c6b77bd10297a8
boodscode237 <boodscode@gmail.com>
2022-11-29 00:56:32 +0500

    Add project files.

---
+++ ConsoleUI04/PersonModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI04
+{
+    public class PersonModel
+    {
+        public string FirstName { private get; set; }
+        public string LastName { get; private set; }
+
+        private string _password;
+
+        public string Password
+        {
+            set { _password = value; }
+        }
+
+        public string FullName 
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        // public int Age { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get 
+            { 
+                return _age; 
+            }
+            set 
+            {
+                if (value >= 0 && value < 146)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("value", "Age should be in the range 0 to 145");
+                }
+            }
+        }
+
+        private string _bankCard;
+
+        public string BankCard
+        {
+            get 
+            {
+                string output = "****-****-****-" + _bankCard.Substring(_bankCard.Length - 4);
+                return output; 
+            }
+            set { _bankCard = value; }
+        }
+
+        public PersonModel(string lastName)
+        {
+            LastName = lastName;
+        }
+        public PersonModel()
+        {
+
+        }
+    }
+}
---
+++ ConsoleUI04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel("Doe");
+
+            person.FirstName = "Joe";
+            person.Age = 25;
+            person.BankCard = "5684-5816-8884-2569";
+
+            Console.WriteLine(person.BankCard);
+            Console.WriteLine(person.FullName);
+
+            Console.ReadLine();
+        }
+    }
+}
